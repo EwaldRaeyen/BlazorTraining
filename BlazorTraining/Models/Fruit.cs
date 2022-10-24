6b4fa805b002f3f823974a91759d10b7a82d1dc1
Ewald Raeyen <ewaldraeyen@gmail.com>
2022-10-24 15:13:47 +0200

    Part 1

---
+++ BlazorTraining/Models/Fruit.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorTraining.Models
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public string Calories { get; set; }
+        public string Family { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
